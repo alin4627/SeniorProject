433fe979756bfccd889aebdd6a320e2438622d49
Jarman <jarman124@gmail.com>
2019-03-12 15:01:19 -0400

    Hide header in login page

--- senior-project-2019/screens/LoginScreen.js
+++ senior-project-2019/screens/LoginScreen.js
@@ -26,6 +26,9 @@ import {
   Content
 } from "native-base";
 class LoginScreen extends React.Component {
+  static navigationOptions = {
+    header: null
+}
   constructor(props) {
     super(props);
     this.state = { 
