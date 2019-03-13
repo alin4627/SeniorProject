dd0921ee7ac8cd12725f574a894e708f5a16110d
Jarman Liu <jarman124@gmail.com>
2019-03-13 17:51:50 -0400

    Center logout button

--- senior-project-2019/screens/SettingsScreen.js
+++ senior-project-2019/screens/SettingsScreen.js
@@ -1,50 +1,54 @@
-import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  Alert
-} from 'react-native';
-import {
-  Header,
-  Left,
-  Body,
-  Title,
-  Right,
-  Button,
-  Icon
-} from 'native-base';
+import React from "react";
+import { StyleSheet, Text, View, Alert } from "react-native";
+import { Header, Left, Body, Title, Right, Button, Icon } from "native-base";
 import * as firebase from "firebase";
 class SettingsScreen extends React.Component {
-
   signOutUser = async () => {
-    await firebase.auth().signOut() 
-    .then(() => { Alert.alert("You succesfully logged out"); 
-    this.props.navigation.navigate('Login');  }, 
-    (error) => { Alert.alert(error.message); });
-  }
+    await firebase
+      .auth()
+      .signOut()
+      .then(
+        () => {
+          Alert.alert("You succesfully logged out");
+          this.props.navigation.navigate("Login");
+        },
+        error => {
+          Alert.alert(error.message);
+        }
+      );
+  };
 
   render() {
     return (
       <View style={styles.container}>
-        <Header style={{backgroundColor: '#fff'}}>
-        <Left>
+        <Header style={{ backgroundColor: "#fff" }}>
+          <Left>
             <Button transparent dark>
-              <Icon name='menu' style={{padding:8}} onPress={() => this.props.navigation.openDrawer()}/>
+              <Icon
+                name="menu"
+                style={{ padding: 8 }}
+                onPress={() => this.props.navigation.openDrawer()}
+              />
               <Title>Settings</Title>
             </Button>
           </Left>
           <Body />
           <Right>
             <Button transparent dark>
-              <Icon name='more' />
+              <Icon name="more" />
             </Button>
           </Right>
         </Header>
-        <View style={{flex: 1,alignItems: 'center', justifyContent:'center'}}>
+        <View
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        >
           <Text>Settings</Text>
-          <Button light onPress={this.signOutUser}>
-                  <Text> Sign Out </Text>
+          <Button
+            light
+            onPress={this.signOutUser}
+            style={{ padding: "10%", alignSelf: "center" }}
+          >
+            <Text> Sign Out </Text>
           </Button>
         </View>
       </View>
@@ -56,6 +60,6 @@ export default SettingsScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1
   }
 });
