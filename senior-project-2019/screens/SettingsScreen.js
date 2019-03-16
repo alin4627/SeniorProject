4a0268afdb7a6e126a05a55ab7c6328c7d88837a
Jarman <jarman124@gmail.com>
2019-03-16 15:51:32 -0400

    Remove sign out button in settings

--- senior-project-2019/screens/SettingsScreen.js
+++ senior-project-2019/screens/SettingsScreen.js
@@ -1,22 +1,7 @@
 import React from "react";
-import { StyleSheet, Text, View, Alert } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { Header, Left, Body, Title, Right, Button, Icon } from "native-base";
-import * as firebase from "firebase";
 class SettingsScreen extends React.Component {
-  signOutUser = async () => {
-    await firebase
-      .auth()
-      .signOut()
-      .then(
-        () => {
-          Alert.alert("You succesfully logged out");
-          this.props.navigation.navigate("Login");
-        },
-        error => {
-          Alert.alert(error.message);
-        }
-      );
-  };
 
   render() {
     return (
@@ -38,13 +23,6 @@ class SettingsScreen extends React.Component {
           style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
         >
           <Text>Settings</Text>
-          <Button
-            light
-            onPress={this.signOutUser}
-            style={{ padding: "10%", alignSelf: "center" }}
-          >
-            <Text> Sign Out </Text>
-          </Button>
         </View>
       </View>
     );
