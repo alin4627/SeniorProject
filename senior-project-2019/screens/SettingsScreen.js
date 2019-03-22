9a23ddde4ff95978be8306fc13f104865a107f66
Jarman <jarman124@gmail.com>
2019-03-22 16:28:00 -0400

    Pull name from database and display in settings

--- senior-project-2019/screens/SettingsScreen.js
+++ senior-project-2019/screens/SettingsScreen.js
@@ -1,7 +1,32 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Header, Left, Body, Title, Right, Button, Icon } from "native-base";
+import * as firebase from 'firebase';
+
 class SettingsScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    const ref = firebase.database().ref('users/' + firebase.auth().currentUser.uid);
+    ref.on("value", snapshot => {
+      let items = snapshot.val();
+      let newState = [];
+      console.log(items)
+      newState.push({
+        id: firebase.auth().currentUser.uid,
+        firstName: items.firstName,
+        lastName: items.lastName
+      });
+      this.setState({
+        items: newState
+      });
+    });
+  }
 
   render() {
     return (
@@ -16,13 +41,22 @@ class SettingsScreen extends React.Component {
               />
             </Button>
           </Left>
-          <Body><Title style={{color: "white" }}>Settings</Title></Body>
+          <Body>
+            <Title style={{color: "white" }}>Settings</Title>
+          </Body>
           <Right />
         </Header>
         <View
           style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
         >
-          <Text>Settings</Text>
+          {this.state.items.map(item => {
+            return (
+              <View key={item.id}>
+                <Title>{item.firstName}</Title>
+                <Title>{item.lastName}</Title>
+              </View>
+            );
+          })}
         </View>
       </View>
     );
