1dd3d3cb2ba1b9dc7eb2be2b973093e2eb121996
Jarman Liu <jarman124@gmail.com>
2019-03-25 15:11:01 -0400

    Bold majors in class list

--- senior-project-2019/screens/ClassSelectionScreen.js
+++ senior-project-2019/screens/ClassSelectionScreen.js
@@ -19,7 +19,7 @@ import {
   ListItem,
   Icon
 } from "native-base";
-import * as firebase from 'firebase';
+import * as firebase from "firebase";
 
 class ClassesSelectionScreen extends React.Component {
   constructor(props) {
@@ -30,23 +30,22 @@ class ClassesSelectionScreen extends React.Component {
   }
 
   componentDidMount() {
-    const ref = firebase.database().ref('Courses/');
+    const ref = firebase.database().ref("Courses/");
     ref.on("value", snapshot => {
       let items = snapshot.val();
       let newState = [];
       var objectKeys = Object.keys(items);
       for (i = 0; i < objectKeys.length; i++) {
         let data = {};
-        data.id = objectKeys[i]
-        for (let item in items[objectKeys[i]])
-        {
+        data.id = objectKeys[i];
+        for (let item in items[objectKeys[i]]) {
           data[item] = {
-              id: items[objectKeys[i]][item].course_id,
-              title: items[objectKeys[i]][item].course_title,
-              category: objectKeys[i]
-          }
+            id: items[objectKeys[i]][item].course_id,
+            title: items[objectKeys[i]][item].course_title,
+            category: objectKeys[i]
+          };
         }
-        newState.push(data)
+        newState.push(data);
       }
       this.setState({
         items: newState
@@ -55,15 +54,23 @@ class ClassesSelectionScreen extends React.Component {
   }
 
   createList = () => {
-    let list = []
+    let list = [];
     for (let i = 0; i < this.state.items.length; i++) {
-      let children = []
+      let children = [];
 
-      for (let item in this.state.items[i])
-      {
-        if (item != 'id') {
-          children.push( 
-            <ListItem key={this.state.items[i][item].id} onPress={() => this.props.navigation.navigate('Class', { title: this.state.items[i][item].title, course_id: this.state.items[i][item].id, category: this.state.items[i][item].category })}>
+      for (let item in this.state.items[i]) {
+        if (item != "id") {
+          children.push(
+            <ListItem
+              key={this.state.items[i][item].id}
+              onPress={() =>
+                this.props.navigation.navigate("Class", {
+                  title: this.state.items[i][item].title,
+                  course_id: this.state.items[i][item].id,
+                  category: this.state.items[i][item].category
+                })
+              }
+            >
               <Left>
                 <Text>{this.state.items[i][item].title}</Text>
               </Left>
@@ -71,42 +78,44 @@ class ClassesSelectionScreen extends React.Component {
                 <Icon name="arrow-forward" />
               </Right>
             </ListItem>
-          )
+          );
         }
       }
       list.push(
         <View key={this.state.items[i].id}>
-          <ListItem itemDivider style={{backgroundColor:'#EEEEEE'}}>
-            <Text>{this.state.items[i].id}</Text>
+          <ListItem itemDivider style={{ backgroundColor: "#EEEEEE" }}>
+            <Text style={{ fontWeight: "bold" }}>{this.state.items[i].id}</Text>
           </ListItem>
           {children}
         </View>
-      )
+      );
     }
-    return list
-  }
+    return list;
+  };
 
   render() {
     return (
       <KeyboardAvoidingView behavior="padding" style={styles.container}>
-        <Header iosBarStyle={"light-content"} style={{ backgroundColor: "#333333" }}>
+        <Header
+          iosBarStyle={"light-content"}
+          style={{ backgroundColor: "#333333" }}
+        >
           <Left>
-            <Button transparent dark onPress={() => this.props.navigation.goBack()}>
-            <Icon
-                name="arrow-back"
-                style={{ padding: 10, color:"white" }}
-              />
+            <Button
+              transparent
+              dark
+              onPress={() => this.props.navigation.goBack()}
+            >
+              <Icon name="arrow-back" style={{ padding: 10, color: "white" }} />
             </Button>
           </Left>
-          <Body><Title style={{color:"white"}}>All Courses</Title></Body>
+          <Body>
+            <Title style={{ color: "white" }}>All Courses</Title>
+          </Body>
           <Right />
         </Header>
-        <Content style={{backgroundColor:"#F8F8F8"}}>
-        <List>
-          {
-            this.createList()
-          }
-        </List>
+        <Content style={{ backgroundColor: "#F8F8F8" }}>
+          <List>{this.createList()}</List>
         </Content>
       </KeyboardAvoidingView>
     );
