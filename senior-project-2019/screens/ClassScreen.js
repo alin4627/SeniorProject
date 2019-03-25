b55a5beaa8816751fdc5ad2207bc11d57fc28fa8
Jarman Liu <jarman124@gmail.com>
2019-03-25 15:04:18 -0400

    Auto add to user count for default group after subscribing

--- senior-project-2019/screens/ClassScreen.js
+++ senior-project-2019/screens/ClassScreen.js
@@ -1,8 +1,5 @@
 import React from "react";
-import {
-  StyleSheet,
-  View,
-} from "react-native";
+import { StyleSheet, View } from "react-native";
 import {
   Header,
   H2,
@@ -15,52 +12,71 @@ import {
   Text,
   Icon
 } from "native-base";
-import * as firebase from 'firebase';
+import * as firebase from "firebase";
 
 class ClassScreen extends React.Component {
   render() {
     const { navigation } = this.props;
-    const title = navigation.getParam('title', 'Unavailable');
-    const course_id = navigation.getParam('course_id', 'Unavailable');
-    const category = navigation.getParam('category', 'Unavailable');
+    const title = navigation.getParam("title", "Unavailable");
+    const course_id = navigation.getParam("course_id", "Unavailable");
+    const category = navigation.getParam("category", "Unavailable");
     return (
       <View behavior="padding" style={styles.container}>
-        <Header iosBarStyle={"light-content"} style={{ backgroundColor: "#333333" }}>
+        <Header
+          iosBarStyle={"light-content"}
+          style={{ backgroundColor: "#333333" }}
+        >
           <Left>
-            <Button transparent dark onPress={() => this.props.navigation.goBack()}>
-              <Icon
-                name="arrow-back"
-                style={{ padding: 10, color: "white" }}
-              />
+            <Button
+              transparent
+              dark
+              onPress={() => this.props.navigation.goBack()}
+            >
+              <Icon name="arrow-back" style={{ padding: 10, color: "white" }} />
             </Button>
           </Left>
-          <Body><Title style={{color: "white" }}>{course_id}</Title></Body>
+          <Body>
+            <Title style={{ color: "white" }}>{course_id}</Title>
+          </Body>
           <Right>
             <Button transparent dark>
-              <Icon ios='md-more' android="md-more" style={{ color: "white" }} />
+              <Icon
+                ios="md-more"
+                android="md-more"
+                style={{ color: "white" }}
+              />
             </Button>
           </Right>
         </Header>
-        <Content padder contentContainerStyle={{ justifyContent: 'center', flex: 1 }} style={{backgroundColor:"#F8F8F8"}}>
-            <View style={styles.center}>
-                <H2 style={styles.textHeaders}>Name: {title}</H2>
-                <H2 style={styles.textHeaders}>Course ID: {course_id}</H2>
-                <Text style={styles.textHeaders}>View past students</Text>
-                <View style={styles.textHeaders}>
-                    <Button style={{alignSelf: "center" }} onPress={() => this.addClass(title, course_id, category)}>
-                        <Text>Request Access</Text>
-                    </Button>
-                </View>
+        <Content
+          padder
+          contentContainerStyle={{ justifyContent: "center", flex: 1 }}
+          style={{ backgroundColor: "#F8F8F8" }}
+        >
+          <View style={styles.center}>
+            <H2 style={styles.textHeaders}>Name: {title}</H2>
+            <H2 style={styles.textHeaders}>Course ID: {course_id}</H2>
+            <Text style={styles.textHeaders}>View past students</Text>
+            <View style={styles.textHeaders}>
+              <Button
+                style={{ alignSelf: "center" }}
+                onPress={() => this.addClass(title, course_id, category)}
+              >
+                <Text>Request Access</Text>
+              </Button>
             </View>
+          </View>
         </Content>
       </View>
     );
   }
 
   addClass(title, course_id, category) {
-    firebase.database()
+    firebase
+      .database()
       .ref(
-        'users/' + firebase.auth().currentUser.uid +
+        "users/" +
+          firebase.auth().currentUser.uid +
           "/classSubscriptions/" +
           title
       )
@@ -68,6 +84,19 @@ class ClassScreen extends React.Component {
         category: category,
         course_title: title,
         course_id: course_id
+      });
+    firebase
+      .database()
+      .ref(
+        "Courses/" +
+          category +
+          "/" +
+          title +
+          "/Groups/Default Group/users/" +
+          firebase.auth().currentUser.uid
+      )
+      .set({
+        userName: firebase.auth().currentUser.displayName
       });
   }
 }
@@ -79,9 +108,9 @@ const styles = StyleSheet.create({
     flex: 1
   },
   center: {
-    alignItems: "center", 
+    alignItems: "center"
   },
   textHeaders: {
-      padding: 20
+    padding: 20
   }
 });
