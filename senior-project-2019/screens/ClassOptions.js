1316b59dad7f6e0900263d7735f140d3602794c2
Jarman Liu <jarman124@gmail.com>
2019-04-01 14:26:15 -0400

    Change is student state

--- senior-project-2019/screens/ClassOptions.js
+++ senior-project-2019/screens/ClassOptions.js
@@ -22,7 +22,7 @@ class ClassScreen extends React.Component {
       title: "",
       course_id: "",
       category: "",
-      isStudent: true
+      isStudent: ""
     };
   }
 
@@ -35,25 +35,78 @@ class ClassScreen extends React.Component {
       title: title,
       course_id: course_id,
       category: category
-    })
-    const ref = firebase.database().ref('users/' + firebase.auth().currentUser.uid);
+    });
+    const ref = firebase
+      .database()
+      .ref("users/" + firebase.auth().currentUser.uid);
     ref.on("value", snapshot => {
       let items = snapshot.val();
-      if(items.userLevel != 1) {
+      if (items.userLevel != 1) {
         this.setState({
           isStudent: false
-        })
+        });
+      } else {
+        this.setState({
+          isStudent: true
+        });
       }
     });
   }
 
   createRosterCategory() {
-    let content = []
-    if (!this.state.isStudent)
-    {
+    console.log(this.state.isStudent);
+    let content = [];
+    if (this.state.isStudent == false) {
       content.push(
         <View key="nonStudentRoster">
-          <ListItem onPress={() => this.props.navigation.navigate('RosterList', { title: this.state.title, course_id: this.state.course_id, category: this.state.category, status: "current" })}>
+          <ListItem
+            onPress={() =>
+              this.props.navigation.navigate("RosterList", {
+                title: this.state.title,
+                course_id: this.state.course_id,
+                category: this.state.category,
+                status: "current"
+              })
+            }
+          >
+            <Left>
+              <Text>View Course Roster</Text>
+            </Left>
+            <Right>
+              <Icon name="arrow-forward" />
+            </Right>
+          </ListItem>
+          <ListItem
+            onPress={() =>
+              this.props.navigation.navigate("RosterList", {
+                title: this.state.title,
+                course_id: this.state.course_id,
+                category: this.state.category,
+                status: "pending"
+              })
+            }
+          >
+            <Left>
+              <Text>View Requested Students</Text>
+            </Left>
+            <Right>
+              <Icon name="arrow-forward" />
+            </Right>
+          </ListItem>
+        </View>
+      );
+    } else {
+      content.push(
+        <ListItem
+          key="rosterList"
+          onPress={() =>
+            this.props.navigation.navigate("RosterList", {
+              title: this.state.title,
+              course_id: this.state.course_id,
+              category: this.state.category
+            })
+          }
+        >
           <Left>
             <Text>View Course Roster</Text>
           </Left>
@@ -61,30 +114,9 @@ class ClassScreen extends React.Component {
             <Icon name="arrow-forward" />
           </Right>
         </ListItem>
-        <ListItem onPress={() => this.props.navigation.navigate('RosterList', { title: this.state.title, course_id: this.state.course_id, category: this.state.category, status: "pending" })}>
-        <Left>
-          <Text>View Requested Students</Text>
-        </Left>
-        <Right>
-          <Icon name="arrow-forward" />
-        </Right>
-      </ListItem>
-        </View>
-      )
+      );
     }
-    else {
-      content.push(
-        <ListItem key="rosterList" onPress={() => this.props.navigation.navigate('RosterList', { title: this.state.title, course_id: this.state.course_id, category: this.state.category })}>
-            <Left>
-              <Text>View Course Roster</Text>
-            </Left>
-            <Right>
-              <Icon name="arrow-forward" />
-            </Right>
-        </ListItem>
-      )
-    }
-    return content
+    return content;
   }
 
   render() {
@@ -97,50 +129,55 @@ class ClassScreen extends React.Component {
               dark
               onPress={() => this.props.navigation.goBack()}
             >
-              <Icon name="arrow-back" style={{ padding: 5}} />
+              <Icon name="arrow-back" style={{ padding: 5 }} />
               <Text>My Courses</Text>
             </Button>
           </Left>
           <Body />
           <Right />
         </Header>
-        <Content
-          padder
-          style={{ backgroundColor: "#F8F8F8" }}
-        >
-        <View style={styles.classHeader}>
+        <Content padder style={{ backgroundColor: "#F8F8F8" }}>
           <View style={styles.classHeader}>
-            <Text style={styles.classLabel}>Class Name</Text>
-            <Text style={styles.classNameHeader}>{this.state.title}</Text>
+            <View style={styles.classHeader}>
+              <Text style={styles.classLabel}>Class Name</Text>
+              <Text style={styles.classNameHeader}>{this.state.title}</Text>
+            </View>
+            <View style={styles.classHeader}>
+              <Text style={styles.classLabel}>Course ID</Text>
+              <Text style={styles.classIDHeader}>{this.state.course_id}</Text>
+            </View>
           </View>
-          <View style={styles.classHeader}>
-            <Text style={styles.classLabel}>Course ID</Text>
-          <Text style={styles.classIDHeader}>{this.state.course_id}</Text>
-          </View>
-        </View>
           <List>
             <ListItem itemHeader first>
-              <Icon name="chatboxes" style={{paddingRight: 10}}/>
+              <Icon name="chatboxes" style={{ paddingRight: 10 }} />
               <Text style={styles.categoryHeader}>GROUPS</Text>
             </ListItem>
-            <ListItem onPress={() => this.props.navigation.navigate('OwnGroups', { title: this.state.title, course_id: this.state.course_id, category: this.state.category })} >
-                <Left>
-                    <Text>My Groups</Text>
-                </Left>
-                <Right>
-                    <Icon name="arrow-forward" />
-                </Right>
+            <ListItem
+              onPress={() =>
+                this.props.navigation.navigate("OwnGroups", {
+                  title: this.state.title,
+                  course_id: this.state.course_id,
+                  category: this.state.category
+                })
+              }
+            >
+              <Left>
+                <Text>My Groups</Text>
+              </Left>
+              <Right>
+                <Icon name="arrow-forward" />
+              </Right>
             </ListItem>
             <ListItem last>
-                <Left>
-                    <Text>Open Groups</Text>
-                </Left>
-                <Right>
-                    <Icon name="arrow-forward" />
-                </Right>
+              <Left>
+                <Text>Open Groups</Text>
+              </Left>
+              <Right>
+                <Icon name="arrow-forward" />
+              </Right>
             </ListItem>
             <ListItem itemHeader>
-              <Icon name="contacts" style={{paddingRight: 10}}/>
+              <Icon name="contacts" style={{ paddingRight: 10 }} />
               <Text style={styles.categoryHeader}>ROSTER</Text>
             </ListItem>
             {this.createRosterCategory()}
@@ -165,12 +202,12 @@ const styles = StyleSheet.create({
     color: "#A8A8A8"
   },
   classNameHeader: {
-    fontSize: 30, 
-    fontWeight:"bold"
+    fontSize: 30,
+    fontWeight: "bold"
   },
   classIDHeader: {
-    fontSize: 25, 
-    fontWeight:"bold"
+    fontSize: 25,
+    fontWeight: "bold"
   },
   center: {
     alignItems: "center"
